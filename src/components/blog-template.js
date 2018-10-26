7975e186650b0a9dc96025ee88d75878cd6d70d9
FURams09 <padin.greg@gmail.com>
2018-10-25 23:00:36 -0400

    added some navigation

--- src/components/blog-template.js
+++ src/components/blog-template.js
@@ -1,50 +1,26 @@
 import React, { Component } from 'react';
-import { graphql, navigate } from 'gatsby';
+import { graphql } from 'gatsby';
 
 import styles from '../styles';
-import { GoogleLogout } from 'react-google-login';
 import Auth from '../lib/auth';
 import Layout from './layout';
 export default class BlogTemplate extends Component {
   constructor(props) {
     super(props);
   }
-  logout() {
-    Auth.get('http://localhost:3030/logout')
-      .then((res) => {
-        console.log(res);
-        localStorage.removeItem('Authorization');
-        navigate('/');
-      })
-      .catch((ex) => {
-        console.log(ex);
-      });
-  }
+
   render() {
     const { data } = this.props;
     const { frontmatter, html } = data.markdownRemark;
     return (
       <Layout showHeader>
         <div style={styles.Grid.gridContainer}>
-          <GoogleLogout
-            style={{ padding: '10px 20px', margin: '10px' }}
-            buttonText="Logout"
-            onLogoutSuccess={this.logout.bind(this)}
-          />
           <div
             style={Object.assign({}, styles.Grid.gridContentArea, {
               gridArea: `aboutMe`,
             })}
           >
             <div dangerouslySetInnerHTML={{ __html: html }} />
-            <button
-              onClick={() => {
-                localStorage.removeItem('Authorization');
-                navigate('/');
-              }}
-            >
-              Kill
-            </button>
           </div>
         </div>
       </Layout>
--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,70 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React, { Component } from 'react';
+import { Link, navigate } from 'gatsby';
 
-const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
+import { GoogleLogout } from 'react-google-login';
+
+import Auth from '../lib/auth';
+class Header extends Component {
+  constructor(props) {
+    super(props);
+  }
+  logout() {
+    Auth.get('http://localhost:3030/logout')
+      .then((res) => {
+        console.log(res);
+        localStorage.removeItem('Authorization');
+        navigate('/');
+      })
+      .catch((ex) => {
+        console.log(ex);
+      });
+  }
+
+  render() {
+    return (
+      <div
+        style={{
+          background: 'rebeccapurple',
+          marginBottom: '1.45rem',
+        }}
+      >
+        <div
           style={{
-            color: 'white',
-            textDecoration: 'none',
+            margin: '0 auto',
+            maxWidth: 960,
+            padding: '1.45rem 1.0875rem',
           }}
         >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </div>
-)
+          <h1 style={{ margin: 0 }}>
+            <Link
+              to="/"
+              style={{
+                color: 'white',
+                textDecoration: 'none',
+              }}
+            >
+              {this.props.siteTitle}
+            </Link>
 
-export default Header
+            <GoogleLogout
+              style={{ padding: '10px 20px', margin: '10px' }}
+              buttonText="Logout"
+              onLogoutSuccess={this.logout.bind(this)}
+            />
+          </h1>
+          <button
+            onClick={() => {
+              localStorage.removeItem('Authorization');
+              navigate('/');
+            }}
+          >
+            Kill
+          </button>
+          <Link to="/admin">Admin</Link>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
